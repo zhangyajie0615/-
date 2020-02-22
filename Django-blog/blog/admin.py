1e5e4dafa5a5f82665721608725c221c7258ff7d
张亚杰 <1581575060@qq.com>
2020-02-22 21:00:00 +0800

    update

--- Django-blog/blog/admin.py
+++ Django-blog/blog/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Post,Category,Tag
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title','created_time','modified_time','category','author']
+    fields = ['title', 'body', 'excerpt', 'category', 'tags']
+
+
+    def save_model(self, request, obj, form, change):
+        obj.author = request.user
+        super().save_model(request, obj, form, change)
+#第一个request是此次HTTP请求对象，第二个obj此次关联对象实例user；super() 函数是用于调用父类(超类)的一个方法
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category)
+admin.site.register(Tag)
--- Django-blog/blog/models.py
+++ Django-blog/blog/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 # Create your models here.
-
+from django.urls import reverse
+from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -16,6 +17,12 @@ class Category(models.Model):
     """
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = '分类'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
 
 class Tag(models.Model):
     """
@@ -23,6 +30,12 @@ class Tag(models.Model):
     再次强调一定要继承 models.Model 类！
     """
     name = models.CharField(max_length=100)
+    class Meta:
+        verbose_name = '标签'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
@@ -31,19 +44,19 @@ class Post(models.Model):
     """
 
     # 文章标题
-    title = models.CharField(max_length=70)
+    title = models.CharField('标题',max_length=70)
 
     # 文章正文，使用 TextField。
     # 存储比较短的字符串可以使用 CharField，但对于文章的正文来说可能会是一大段文本，因此使用 TextField 来存储大段文本。
-    body = models.TextField()
+    body = models.TextField('正文')
 
     # 这两个列分别表示文章的创建时间和最后一次修改时间，存储时间的字段用 DateTimeField 类型。
-    created_time = models.DateTimeField()
-    modified_time = models.DateTimeField()
+    created_time = models.DateTimeField('创建时间',default=timezone.now)
+    modified_time = models.DateTimeField('修改时间')
 
     # 文章摘要，可以没有文章摘要，但默认情况下 CharField 要求我们必须存入数据，否则就会报错。
     # 指定 CharField 的 blank=True 参数值后就可以允许空值了。
-    excerpt = models.CharField(max_length=200, blank=True)
+    excerpt = models.CharField('摘要',max_length=200, blank=True)
 
     # 这是分类与标签，分类与标签的模型我已经定义在上面。
     # 这里把文章对应的数据库表和分类、标签对应的数据库表关联了起来，但是关联形式稍微有点不同。
@@ -54,8 +67,8 @@ class Post(models.Model):
     # 而对于标签来说，一篇文章可以有多个标签，同一个标签下也可能有多篇文章，所以我们使用
     # ManyToManyField，表明这是多对多的关联关系。
     # 同时我们规定文章可以没有标签，因此为标签 tags 指定了 blank=True。
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    tags = models.ManyToManyField(Tag, blank=True)
+    category = models.ForeignKey(Category, verbose_name='分类',on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag,'标签', blank=True)
 
     # 文章作者，这里 User 是从 django.contrib.auth.models 导入的。
     # django.contrib.auth 是 django 内置的应用，专门用于处理网站用户的注册、登录等流程，User 是
@@ -63,4 +76,17 @@ class Post(models.Model):
     # 这里我们通过 ForeignKey 把文章和 User 关联了起来。
     # 因为我们规定一篇文章只能有一个作者，而一个作者可能会写多篇文章，因此这是一对多的关联关系，和
     # Category 类似。
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, verbose_name='作者',on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = '文章'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.title
+
+    def save(self,*args,**kwargs):
+        self.modified_time=timezone.now()
+        super().save(*args,**kwargs)
+
+    def get_absolute_url(self):
+        return reverse('blog:detail',kwargs={'pk':self.pk})
